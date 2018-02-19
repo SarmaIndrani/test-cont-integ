3f60f5bfcf83434f9ed44a781e679425d4471576
SarmaIndrani <tech.indrani@gmail.com>
2018-02-19 16:08:53 +0530

    first push

---
+++ test/test.js
@@ -0,0 +1,24 @@
+
+var supertest = require("supertest");
+var should = require("should");
+
+//This agent refers to PORT where the program is running.
+
+var server = supertest.agent("http://localhost:4000");
+
+//UNIT test begin
+
+describe("SAMPLE UNIT TEST ", function(){
+	// #1 should return home page
+	it("should return home page", function(done){
+		// calling home page
+		server.get("/")
+		.expect("content-type", /text/)
+		.expect(200) // http response
+		.end(function(err, res){
+			res.status.should.equal(200);
+			done();
+		});
+	});
+	
+});
