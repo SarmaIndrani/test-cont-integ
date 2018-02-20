ec62c7bd8f822e418276a232aa39f118f817919d
SarmaIndrani <tech.indrani@gmail.com>
2018-02-20 19:39:06 +0530

    log added 3

--- test/test.js
+++ test/test.js
@@ -12,7 +12,7 @@ describe("SAMPLE UNIT TEST ", function(){
 	// #1 should return home page
 	it("should return home page", function(done){
 		// calling home page
-		console.log('Sample test 2');
+		console.log('Sample test 3');
 		server.get("/")
 		.expect("content-type", /text/)
 		.expect(200) // http response
